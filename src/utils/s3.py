1159c100f44f090fc879a3e13078c4e9101340d9
Ubuntu <ubuntu@ip-172-31-29-245.eu-west-1.compute.internal>
2022-10-19 04:21:40 +0000

    comment out the S3 address

--- src/utils/s3.py
+++ src/utils/s3.py
@@ -17,7 +17,7 @@ session = boto3.session.Session()
 # Client, used to upload file
 s3_client = session.client("s3",
     region_name=S3_REGION,
-    endpoint_url=S3_ENDPOINT,
+    #endpoint_url=S3_ENDPOINT,
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
@@ -26,7 +26,7 @@ s3_client = session.client("s3",
 s3_resource = boto3.resource(
     "s3",
     region_name=S3_REGION,
-    endpoint_url=S3_ENDPOINT,
+    #endpoint_url=S3_ENDPOINT,
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
